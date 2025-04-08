7ee42d5a7b78865148153a568b25a2f764c88406
limkyulee <mud0308@gmail.com>
2025-04-08 10:20:59 +0900

    frontController v4

---
+++ src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -0,0 +1,71 @@
+package hello.servlet.web.frontcontroller.v4;
+
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+//  REFACTOR : controller 마다 modelView 생성하는 로직 삭제.
+@WebServlet(name="frontControllerServletV4", urlPatterns = "/front-controller/v4/*")
+public class FrontControllerServletV4 extends HttpServlet {
+
+    private Map<String, ControllerV4> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV4() {
+        controllerMap.put("/front-controller/v4/members/new-form", new MemberFormControllerV4());
+        controllerMap.put("/front-controller/v4/members/save", new MemberSaveControllerV4());
+        controllerMap.put("/front-controller/v4/members", new MemberListControllerV4());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("FrontControllerServletV4.service");
+
+        String requestURI = request.getRequestURI();
+        ControllerV4 controllerV4 = controllerMap.get(requestURI);
+
+        if (controllerV4 == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND); // 404
+
+            return;
+        }
+
+//      수정된 영역. | 모델을 생성하여 controller 에 저장.
+        Map<String, String> paramMap = createParamMap(request);
+        Map<String, Object> model = new HashMap<>();
+//      view 경로를 반환.
+        String viewName = controllerV4.process(paramMap, model);
+
+        MyView view = viewResolver(viewName);
+        view.render(model, request, response);
+    }
+
+    private static MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+
+    private static Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames()
+                .asIterator()
+                .forEachRemaining(
+                    paramName -> paramMap.put(
+                            paramName,
+                            request.getParameter(paramName)
+                    )
+        );
+
+        return paramMap;
+    }
+}
